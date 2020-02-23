48bfbb35d76b8cde978a4000cf9f3d2555956c7e
CurryRice233 <nmeia@qq.com>
2020-02-23 18:09:42 +0100

    DVI menu

--- DVI/GDD.md
+++ DVI/GDD.md
@@ -10,55 +10,29 @@
 |  Chrome | 0.0.1   | Roguelike & Soulslike | Phaser | 16~30
 
 ## Contenido:
-> sdasrgt
+
+Edad media, se está propagando una enfermedad.
 
 ## Mecánica:
-* Control:
-* 
+
+* Correr 
+* Rodar
+* Golpear
+* Bloquear
+
 
 ## Interfaces:
-* ## Nombre de la Pantalla: 
+| Nombre de la Pantalla | Descripción |
+| :---: | :---: |
+|  Menú Principal |  Botón de jugar |
+| H.U.D. | Vida, Stamina, Corrupción, Items |
+| Menú de Pausa | Botón de cotinuar, salir  |
 
-    El nombre de la pantalla, si es el Menú Principal o el H.U.D. (Heads-up Display).
-* ## Descripción de la Pantalla:
-
-    ¿Para qué sirve esta interface?
-
-* ## Estados del Juego:
-
-    * Menú Principal:
-    * Menú de Pausa:
 
 
 ## Niveles:
-* ## Título del Nivel: 
-
-    El nombre del nivel.
-
-* ## Encuentro: 
-
-    Describir si es el primer nivel, un tutorial o un bonus, en otras palabras, ¿Cuándo es que el jugador llega a este nivel? Descripción Una descripción detallada del nivel.
-
-* ## Objetivos: 
-
-    ¿Qué debe de hacer el jugador para terminar el nivel? Este campo también debe incluir si el jugador tiene que resolver ciertos acertijos o derrotar a cierto enemigo para progresar. Progreso Describir que ocurre cuando el jugador termina el nivel.
-
-* ## Enemigos: 
-
-    Si el nivel tiene enemigos que el jugador debe enfrentar, éstos se enlistan en este campo, de lo contrario este campo puede ser omitido.
-
-* ## Items: 
-
-    Enumerar los objetos que el jugador o los enemigos pueden usar y que aparecen en este nivel, este campo se puede omitir si no existen dichos objetos.
-
-* ## Personajes: 
-
-    Los personajes que aparecen en el nivel, de igual manera, este campo puede ser omitido si no existen personajes en el juego.
-
-* ## Música y Efectos de Sonido: 
-    
-    Describir la música de este nivel al igual que los efectos de sonido de ambiente que contiene.
-
-* ## Referencias de BGM y SFX: 
-
-    Escribir todas las referencias que apliquen con respecto a la música de fondo y efectos de sonido descritos en la sección de Música y Sonidos.
+| Título del Nivel | Encuentro | Objetivos | Enemigos | Items | Personajes | Música |
+| :---: | :---: | :---: | :---: | :---: | :---: | :---: | 
+| Camino | Empiezo | Avanzar hacia la cuerva | Zombie | Espada Escudo | Protagonista | Estilo Oscuro | 
+| Cuerva | Después de tutorial | Buscar la cura | Zombie | Espada Escudo | Protagonista | Estilo Oscuro | 
+| Jefe Final | Después de cuerva | Obtener la cura | Jefe final | Espada Escudo Cura | Protagonista | Estilo Oscuro | 
--- DVI/src/game.js
+++ DVI/src/game.js
@@ -1,19 +1,104 @@
-
 export default class Game extends Phaser.Scene {
   constructor() {
     super({ key: 'main' });
   }
+
   preload() {
-    this.load.image("logo", "assets/favicon.png")
+    this.load.image("logo", "/assets/favicon.png")
+    this.load.image("fondo", "/assets/fondo_1.png")
+    this.load.image("button_play", "/assets/PlayButton.png")
+    this.load.image("button_exit", "/assets/ExitButton.png")
+    this.load.audio("medieval_m", [
+      "/assets/audio/setting-sail-per-kiilstofte.ogg", 
+      "/assets/audio/setting-sail-per-kiilstofte.mp3"
+    ]);
   }
 
   create() {
-    this.logo = this.add.image(this.game.config.width/2, this.game.config.height/2, "logo")
-    this.logo.displayHeight=200
-    this.logo.displayWidth =135
+    menu(this)
+    
+    function menu (game) {
+
+      //Crear escena
+      let key = "Menu";
+      let autoStart = true;
+      let sceneConfig = {
+        key: 'Menu',
+        // active: false,
+        // visible: true,
+        // pack: false,
+        // cameras: null,
+        // map: {},
+        // physics: {},
+        // loader: {},
+        // plugins: false,
+        // input: {}
+    };
+      let newScene = game.scene.add(key, sceneConfig, autoStart);
+
+      //Fondo
+      newScene.fondo = newScene.add.image(newScene.game.config.width/2, newScene.game.config.height/2, "fondo");
+      newScene.fondo.displayHeight = 956;
+      newScene.fondo.displayWidth = 1433;
+
+      //Musica
+      let a_config = {
+        mute: false,
+        volume: 0.2,
+        rate: 1,
+        detune: 0,
+        seek: 0,
+        loop: true,
+        delay: 0
+      };
+      let music = newScene.sound.add('medieval_m', a_config);
+      music.play();
+
+      //Boton iniciar
+      newScene.playbutton = newScene.add.image(newScene.game.config.width/2, newScene.game.config.height*0.15, "button_play");
+      newScene.playbutton.displayWidth = newScene.game.config.width/8;
+      newScene.playbutton.displayHeight = newScene.game.config.height/8;
+      newScene.playbutton.setInteractive();
+      newScene.playbutton.on('pointerdown', function(){
+        //do things on click
+        music.stop();
+        newScene.scene.remove("Menu");
+        jugar(game);
+      });
+  
+    }
+
+    function jugar (game) {
+      //Crear escena
+      let key = "Inicio"
+      let autoStart = true
+      let sceneConfig = {
+        key: 'Inicio',
+        // active: false,
+        // visible: true,
+        // pack: false,
+        // cameras: null,
+        // map: {},
+        // physics: {},
+        // loader: {},
+        // plugins: false,
+        // input: {}
+    };
+      let initScene = game.scene.add(key, sceneConfig, autoStart);
+
+      //Boton salir
+      initScene.playbutton = initScene.add.image(game.game.config.width/2, game.game.config.height/2, "button_exit");
+      initScene.playbutton.setInteractive();
+      initScene.playbutton.on('pointerdown', function(){
+        //do things on click
+        initScene.scene.remove("Inicio");
+        menu(game);
+      });
+    }
   }
 
   update(time, delta) {
-    this.logo.rotation += 0.07
+
   }
+
 }
